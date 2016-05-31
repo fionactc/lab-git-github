3d37b1ab8c595b089f2e7c158a969bae64ac7c75
Angel Lo <angello@AngeldeMacBook-Pro-2.local>
2016-05-31 16:27:30 +0800

    finished

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+
+//Added by Angel
+
+window.onload = function(){
+  var button = document.getElementById("button-yellow");
+  button.addEventListener("click", function(event){
+    prompt('What is your email?')
+  });
+}
+
 //Javascript to be added
+
+
+//Added by Angel
